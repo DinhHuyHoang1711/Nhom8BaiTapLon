61c82df1f5c21a5d0c0f320d6c9b6459f8a15e40
Do Duc Kiet <dokiet2k6@gmail.com>
2025-10-26 08:54:30 +0700

    feat: add OwnedManager class to manage owned balls and artifacts

---
+++ src/OwnedManager.java
@@ -0,0 +1,81 @@
+package arkanoid;
+
+import java.util.ArrayList;
+import java.util.List;
+import arkanoid.Ball;
+import arkanoid.Artifact;
+
+public class OwnedManager {
+    private List<Ball> balls = new ArrayList<>();
+    private List<Artifact> artifacts = new ArrayList<>();
+
+    private Ball currentBall;
+    private Artifact currentArtifact;
+
+    public  OwnedManager() {
+        this.currentBall = Ball.normalBall(0, 0);
+        this.currentArtifact = null;
+        loadBalls();
+    }
+
+    public OwnedManager(Ball currentBall, Artifact currentArtifact) {
+        this.currentBall = currentBall;
+        this.currentArtifact = currentArtifact;
+    }
+
+    public Ball getCurrentBall() {
+        return currentBall;
+    }
+
+    public void setCurrentBall(Ball currentBall) {
+        this.currentBall = currentBall;
+    }
+
+    public Artifact getCurrentArtifact() {
+        return currentArtifact;
+    }
+
+    public void setCurrentArtifact(Artifact currentArtifact) {
+        this.currentArtifact = currentArtifact;
+    }
+
+    public List<Artifact> getArtifacts() {
+        return artifacts;
+    }
+
+    public List<Ball> getBalls() {
+        return balls;
+    }
+
+    public void addBall(Ball ball) {
+        if (!balls.contains(ball)) {
+            balls.add(ball);
+        }
+    }
+
+    public void addArtifact(Artifact artifact) {
+        if (!artifacts.contains(artifact)) {
+            artifacts.add(artifact);
+        }
+    }
+
+    public void removeBall(Ball ball) {
+        balls.remove(ball);
+    }
+
+    public void removeArtifact(Artifact artifact) {
+        artifacts.remove(artifact);
+    }
+
+    public List<Ball> getOwnedBalls() {
+        return balls;
+    }
+
+    private void loadBalls() {
+        balls.add(Ball.normalBall(0, 0));
+        balls.add(Ball.fireBall(0, 0));
+        balls.add(Ball.earthBall(0, 0));
+        balls.add(Ball.waterBall(0, 0));
+        balls.add(Ball.windBall(0, 0));
+    }
+}
